a10aadb1bdfd843eee7e2e857ef574885a563a95
Desmond Fitch <desmond.fitch@icloud.com>
2025-03-22 17:31:14 -0400

    Initial Commit

---
+++ ArchitectureBootcamp/ArchitectureBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArchitectureBootcampApp.swift
+//  ArchitectureBootcamp
+//
+//  Created by Desmond Fitch on 3/22/25.
+//
+
+import SwiftUI
+
+@main
+struct ArchitectureBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ArchitectureBootcamp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ArchitectureBootcamp
+//
+//  Created by Desmond Fitch on 3/22/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
